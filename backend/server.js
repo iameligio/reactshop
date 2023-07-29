29089736d58fff242a1c5ff1863f01204a9b33a9
Eligio Tapiador Jr <etapiador@gmail.com>
2023-07-30 02:30:31 +0800

    setup node/express backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import products from "./data/products.js";
+
+const port = 5010;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  console.log(products);
+  res.json(products);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
